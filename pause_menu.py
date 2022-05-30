03512bfae986a69d2679502c218ded6d931bf365
Daniel Tower <76547158+RedInfinity-source@users.noreply.github.com>
2022-05-30 18:54:33 -0400

    Update pause_menu.py

--- pause_menu.py
+++ pause_menu.py
@@ -1,21 +1,19 @@
 from ursina import *
 
-# PauseMenu
-class PauseMenu(Entity):
-    def __init__(self):
-        super().__init__(parent=camera.ui)
+class MainMenu(Entity):
+    def __init__(self, player):
+        super().__init__(parent = camera.ui)
 
-        self.pause_menu = Entity(parent=self,enabled = True)
-        self.player = None
-        self.main_menu = None
+        self.main_menu = Entity(parent = self, enabled = True)
+        self.player = player
         mouse.locked = False
 
-        def play():
-            mouse.position = (0, 0, 0)
+        def Play():
+            self.player.enable()
             mouse.locked = True
-            self.pause_menu.disable()
+            self.main_menu.disable()
 
-        resume_button = Button(text="Resume", color=color.black, scale_y=0.1, scale_x=0.3, y=0.12,parent=self.pause_menu)
-        resume_button.on_click = Func(play)
-        quit_button = Button(text="Quit", color=color.black, scale_y=0.1, scale_x=0.3, y=-0.1,parent=self.pause_menu)
+        story_button = Button(text = "Play", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.02, parent = self.main_menu)
+        quit_button = Button(text = "Quit", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.22, parent = self.main_menu)
         quit_button.on_click = application.quit
+        story_button.on_click = Func(Play)
